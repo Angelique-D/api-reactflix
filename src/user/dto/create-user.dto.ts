2c1754158725d8547532998fbf41053007f7fe67
Angelique-D <angelique.didillon@gmail.com>
2024-07-16 15:44:42 +0200

    ✅ add user dto

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsOptional, MinLength, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(15)
+  @ApiProperty()
+  password: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+  
+  @IsOptional()
+  @ApiProperty({ required: false })
+  avatar?: string;
+
+  @IsBoolean()
+  @ApiProperty({ default: false })
+  isAdmin: boolean = false;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updateAt: Date;
+}
